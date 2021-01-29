50a64a2a7881b9e8cb66cc406596d30083d025eb
abarruffo@sysmanagement.it <abarruffo@sysmanagement.it>
2021-01-29 17:53:04 +0100

    styles

--- src/app/drinks/drinks.page.ts
+++ src/app/drinks/drinks.page.ts
@@ -1,42 +1,47 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { IonSearchbar, Platform } from '@ionic/angular';
-import { DrinksService } from '../drinks.service';
-import { Drink } from '../model/drink';
-
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  Optional,
+  ViewChild,
+} from "@angular/core";
+import { Router } from "@angular/router";
+import { Keyboard } from "@ionic-native/keyboard/ngx";
+import { Platform } from "@ionic/angular";
+import { DrinksService } from "../drinks.service";
+import { Drink } from "../model/drink";
 
 @Component({
-  selector: 'app-drinks',
-  templateUrl: './drinks.page.html',
-  styleUrls: ['./drinks.page.scss'],
+  selector: "app-drinks",
+  templateUrl: "./drinks.page.html",
+  styleUrls: ["./drinks.page.scss"],
 })
 export class DrinksPage implements OnInit, AfterViewInit {
   drinks: Drink[];
   searchText: string;
-  @ViewChild('searchBar') searchBar: IonSearchbar;
-  constructor(public platform: Platform, private drinksService: DrinksService, private router: Router) { }
-  ngAfterViewInit(): void {
+  constructor(
+    @Optional() private keyboard: Keyboard,
+    public platform: Platform,
+    private drinksService: DrinksService,
+    private router: Router
+  ) {}
+  ngAfterViewInit(): void {}
 
-    console.log(this.searchBar);
+  ngOnInit() {}
+
+  searchDrink() {
+    this.keyboard.hide();
+    this.getDrinks(this.searchText);
   }
-
-  ngOnInit() {
-    this.getDrinks('margarita');
-   
-  }
-
-
   getDrinks(name: string) {
-    this.drinksService.getDrinksByName(name).
-    subscribe(
+    this.drinksService.getDrinksByName(name).subscribe(
       (res: Drink[]) => {
         this.drinks = res;
       },
-      err => console.error(err)
-    )
+      (err) => console.error(err)
+    );
   }
   goToDetail(drink: Drink) {
-    this.router.navigateByUrl('drinks/'+drink.idDrink,{state: drink});
+    this.router.navigateByUrl("drinks/" + drink.idDrink, { state: drink });
   }
-
 }
